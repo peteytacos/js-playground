21d46be9ffb78c8fb209a2156bbae9aa33e70ecf
Pete Wallroth <p.wallroth@yahoo.com>
2024-02-11 10:39:54 +1100

    Operator math

--- javascript.js
+++ javascript.js
@@ -10,4 +10,16 @@
 
 // let currentUserName = "John";
 
-console.log(23 + 97);
+// console.log(23 + 97);
+
+// let vacationSpots = ["tokyo", "boston", "los angeles", "sydney"];
+
+// console.log(vacationSpots);
+
+// let vacationSpots = ["tokyo", "boston", "los angeles", "sydney"];
+
+// console.log(vacationSpots);
+
+// let x = 0.2 + 0.1;
+
+// console.log(x);
