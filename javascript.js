63db1aa62dc8750e64e06ee6cdb0f0d8817c47f2
Pete Wallroth <p.wallroth@yahoo.com>
2024-02-11 13:33:30 +1100

    Boolean example

--- javascript.js
+++ javascript.js
@@ -6,30 +6,61 @@
 
 // alert(admin);
 
+//
+
 // let ourPlanetName = "Earth";
 
 // let currentUserName = "John";
 
+//
+
 // console.log(23 + 97);
 
-// let vacationSpots = ["tokyo", "boston", "los angeles", "sydney"];
-
-// console.log(vacationSpots);
+//
 
 // let vacationSpots = ["tokyo", "boston", "los angeles", "sydney"];
 
 // console.log(vacationSpots);
+
+// let vacationSpots = ["tokyo", "boston", "los angeles", "sydney"];
+
+// console.log(vacationSpots);
+
+//
 
 // let x = 0.2 + 0.1;
 
 // console.log(x);
 
+//
+
 // let a = 10;
 
 // console.log(a);
+
+//
 
 // function favoriteAnimal(animal) {
 //   return animal + " is my favorite animal!";
 // }
 
 // console.log(favoriteAnimal("Goat"));
+
+//
+
+// alert("not a number" / 2);
+
+//
+
+// let name = "John";
+
+// // embed a variable
+// alert(`Hello, ${name}!`); // Hello, John!
+
+//boolean values
+
+// let isGreater = 4 > 1;
+
+// alert(isGreater); // true (the comparison result is "yes")
+
+//
