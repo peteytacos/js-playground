3fa4a363aa888c5da7210f6ba3772bf4eb3ace71
Pete Wallroth <p.wallroth@yahoo.com>
2024-02-12 09:27:10 +1100

    Logical operators

--- javascript.js
+++ javascript.js
@@ -80,6 +80,24 @@
 //   alert("truthy!");
 // }
 
-//
+//Logical operators
+
+// let userName = prompt("Who's there?", "");
+
+// if (userName === "Admin") {
+//   let pass = prompt("Password?", "");
+
+//   if (pass === "TheMaster") {
+//     alert("Welcome!");
+//   } else if (pass === "" || pass === null) {
+//     alert("Canceled");
+//   } else {
+//     alert("Wrong password");
+//   }
+// } else if (userName === "" || userName === null) {
+//   alert("Canceled");
+// } else {
+//   alert("I don't know you");
+// }
 
 //
