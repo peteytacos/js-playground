9263021427559b8249b637b572251d7c37e83aab
Pete Wallroth <p.wallroth@yahoo.com>
2024-02-11 11:00:27 +1100

    Function practice

--- javascript.js
+++ javascript.js
@@ -23,3 +23,13 @@
 // let x = 0.2 + 0.1;
 
 // console.log(x);
+
+// let a = 10;
+
+// console.log(a);
+
+// function favoriteAnimal(animal) {
+//   return animal + " is my favorite animal!";
+// }
+
+// console.log(favoriteAnimal("Goat"));
