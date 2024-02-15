593f0259316f0fa10eda640e5d41dc303667f6f4
Pete Wallroth <p.wallroth@yahoo.com>
2024-02-15 13:36:04 +1100

    Fizzbuzz second attempt

--- javascript.js
+++ javascript.js
@@ -203,3 +203,19 @@
 //     console.log(i);
 //   }
 // }
+
+//fizzbuzz second attempt
+let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+
+for (let i = 1; i <= answer; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("fizzbuzz");
+  } else if (i % 3 === 0) {
+    console.log("fizz");
+  }
+  else if (i % 5 === 0) {
+    console.log("buzz");
+  } else {
+    console.log(i);
+  }
+}
