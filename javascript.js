78cc422e8415fc64dc4015c0b3b03ab157eac1bf
Pete Wallroth <p.wallroth@yahoo.com>
2024-02-16 11:45:41 +1100

    Add defer to js script in head

--- javascript.js
+++ javascript.js
@@ -205,17 +205,21 @@
 // }
 
 //fizzbuzz second attempt
-let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+// let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
 
-for (let i = 1; i <= answer; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log("fizzbuzz");
-  } else if (i % 3 === 0) {
-    console.log("fizz");
-  }
-  else if (i % 5 === 0) {
-    console.log("buzz");
-  } else {
-    console.log(i);
-  }
-}
+// for (let i = 1; i <= answer; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("fizzbuzz");
+//   } else if (i % 3 === 0) {
+//     console.log("fizz");
+//   }
+//   else if (i % 5 === 0) {
+//     console.log("buzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+//button alert js
+const btn = document.querySelector('#btn');
+btn.onclick = () => alert("Hello World");
